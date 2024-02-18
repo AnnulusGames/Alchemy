c276ad45a5864d7f1af6395ffc0d15f12a76dd4a
AnnulusGames <annulusgames@gmail.com>
2024-02-18 11:54:40 +0900

    Add: summary to hierarchy object classes

--- Alchemy/Assets/Alchemy/Runtime/Hierarchy/HierarchyObjectMode.cs
+++ Alchemy/Assets/Alchemy/Runtime/Hierarchy/HierarchyObjectMode.cs
@@ -1,9 +1,21 @@
 namespace Alchemy.Hierarchy
 {
+    /// <summary>
+    /// Specify how to handle HierarchyObject at runtime.
+    /// </summary>
     public enum HierarchyObjectMode
     {
+        /// <summary>
+        /// Treated as a regular GameObject.
+        /// </summary>
         None = 0,
+        /// <summary>
+        /// Removed in play mode.
+        /// </summary>
         RemoveInPlayMode = 1,
+        /// <summary>
+        /// Removed in build.
+        /// </summary>
         RemoveInBuild = 2
     }
 }
--- Alchemy/Assets/Alchemy/Runtime/Hierarchy/HierarchySeparator.cs
+++ Alchemy/Assets/Alchemy/Runtime/Hierarchy/HierarchySeparator.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace Alchemy.Hierarchy
 {
+    /// <summary>
+    /// A component that displays separators in a hierarchy.
+    /// </summary>
     [AddComponentMenu("Alchemy/Hierarchy Separator")]
     public sealed class HierarchySeparator : HierarchyObject { }
 }
