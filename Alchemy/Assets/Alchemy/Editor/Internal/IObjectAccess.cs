18a770a53f583e25e6620f4787badd9cb594095c
akeit0 <akitotty2@outlook.jp>
2024-02-21 16:30:53 +0900

    Fix : Support to read/write struct in attributes.

---
+++ Alchemy/Assets/Alchemy/Editor/Internal/IObjectAccess.cs
@@ -0,0 +1,48 @@
+﻿using UnityEditor;
+
+namespace Alchemy.Editor
+{
+    public interface IObjectAccess
+    {
+        public object Target { get; set; }
+
+        public static IObjectAccess Create(SerializedProperty property)
+        {
+            var value = property.GetValue<object>();
+            if (value.GetType().IsValueType)
+            {
+                return new SerializedStructAccess(property);
+            }
+            else
+            {
+                return new IdentityAccess(value);
+            }
+        }
+    }
+
+    public class IdentityAccess : IObjectAccess
+    {
+        public object Target { get; set; }
+
+        public IdentityAccess(object target)
+        {
+            Target = target;
+        }
+    }
+
+    public class SerializedStructAccess : IObjectAccess
+    {
+        public SerializedStructAccess(SerializedProperty property)
+        {
+            this.property = property;
+        }
+
+        readonly SerializedProperty property;
+
+        public object Target
+        {
+            get => property.GetValue<object>();
+            set => property.SetValue<object>(value);
+        }
+    }
+}
