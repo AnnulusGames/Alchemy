ae98f77017360bbc1f95ab1c665d60bfa28b08a6
yuyu0127 <yuyut@ymail.ne.jp>
2024-03-17 20:24:40 +0900

    Add: Sample LabelText for list fields

--- Alchemy/Assets/Alchemy/Samples~/Samples/Scripts/Decorations/LabelTextSample.cs
+++ Alchemy/Assets/Alchemy/Samples~/Samples/Scripts/Decorations/LabelTextSample.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Alchemy.Inspector;
 
@@ -8,5 +9,9 @@ namespace Alchemy.Samples
         [LabelText("FOO!")] public float foo;
         [LabelText("BAR!")] public Vector3 bar;
         [LabelText("BAZ!")] public GameObject baz;
+
+        [LabelText("FOO LIST!")] public List<float> fooList;
+        [LabelText("BAR LIST!")] public List<Vector3> barList;
+        [LabelText("BAZ LIST!")] public List<GameObject> bazList;
     }
 }
